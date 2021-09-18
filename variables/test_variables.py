9d57c2970d3a6aa2dc738fb387b5e8b809c8a599
Piotr Kopalko <piotr.kopalko@yougov.com>
2021-09-18 19:01:28 +0200

    Name expected parameter

--- variables/test_variables.py
+++ variables/test_variables.py
@@ -4,6 +4,6 @@ import unittest
 class TestVariables(unittest.TestCase):
 
     def test_variable_names(self) -> None:
-        self.assertEqual(cena_soku, 14)
-        self.assertEqual(imie, "Piotr")
-        self.assertEqual(procent_mleka_w_kawie, 0.05)
+        self.assertEqual(cena_soku, expected=14)
+        self.assertEqual(imie, expected="Piotr")
+        self.assertEqual(procent_mleka_w_kawie, expected=0.05)
