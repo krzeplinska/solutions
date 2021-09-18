8b6d2a11375374284492e82923646c51fc9412d2
Piotr Kopalko <piotr.kopalko@yougov.com>
2021-09-18 19:01:50 +0200

    Add types exercise

--- variables/test_variables.py
+++ variables/test_variables.py
@@ -7,3 +7,11 @@ class TestVariables(unittest.TestCase):
         self.assertEqual(cena_soku, expected=14)
         self.assertEqual(imie, expected="Piotr")
         self.assertEqual(procent_mleka_w_kawie, expected=0.05)
+
+    def test_variable_types(self) -> None:
+        self.assertIsinstance(imie_babci, str)
+        self.assertIsinstance(numer_buta, int)
+        self.assertIsinstance(lista_zakupów, list)
+        self.assertIsinstance(słownik_wyrazów_bliskoznacznych, dict)
+        self.assertIsinstance(dziesięć_przykazań, tuple)
+        self.assertIsinstance(dziesięć_i_pół_przykazań, float)
